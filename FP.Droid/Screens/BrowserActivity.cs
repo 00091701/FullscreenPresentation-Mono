c313012346a6c1a196bc849a69f8e3206ac7c912
David Hoffmann <fullscreen-presentation@hoffmann-projects.de>
2012-10-23 22:19:34 +0200

    Android-Projekt hinzugefügt.
    Signed-off-by: David Hoffmann <fullscreen-presentation@hoffmann-projects.de>

---
+++ FP.Droid/Screens/BrowserActivity.cs
@@ -0,0 +1,46 @@
+/*
+ * This file is part of Fullscreen-Presentation
+ * Copyright (C) 2012 David Hoffmann
+ *
+ * Fullscreen-Presentation is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * Fullscreen-Presentation is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AFullscreen-Presentation; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace De.Dhoffmann.Mono.FullscreenPresentation.Screens
+{
+	[Activity (Label = "BrowserActivity")]			
+	public class BrowserActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create your application here
+		}
+	}
+}
+
